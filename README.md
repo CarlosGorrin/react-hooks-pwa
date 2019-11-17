a3cc7b84b51587f602e9bfd598ea46dd35896eb3
Carlos Gorrin <carlos.gorrin@gmail.com>
2019-11-17 03:01:06 -0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-hooks-pwa
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function App() {
+  const [data, setData] = useState([])
+
+    useEffect(() => {
+      const fetchData = async () => {
+        const result = await fetch('https://orangevalleycaa.org/api/videos')
+          .then(
+            response => response.json()
+          )
+          setData(result);
+      }
+      fetchData();
+    }, []);
+
+  return (
+    <div className="App">
+      <header>
+      <h1>Videos</h1>
+      </header>
+      {data.map(video => (
+        <div key={video.id}>
+          <h2>{video.name}</h2>
+          <video height={200} controls src={video.video_url}/>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
